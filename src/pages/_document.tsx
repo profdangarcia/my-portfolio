2c8562ed3c8fe0bd787513753142e2e6e465d379
Daniel Garcia <daniel.of@gmail.com>
2020-10-29 18:10:46 -0300

    feat: adding favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -42,6 +42,7 @@ export default class MyDocument extends Document {
       <Html lang="pt">
         <Head>
           <meta charSet="utf-8" />
+          <title>Dan Garcia - Fullstack Developer</title>
           <link
             href="https://fonts.googleapis.com/css?family=Poppins"
             rel="stylesheet"
@@ -54,7 +55,7 @@ export default class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Six+Caps&display=swap"
             rel="stylesheet"
           ></link>
-          <link rel="favicon" href="" />
+          <link rel="favicon" href="/favicon.ico" />
         </Head>
         <body>
           <Main />
