3f9bfb7b30f29dc3cc574d60b73d5f3dcefe4e95
Dan Garcia <daniel.of@gmail.com>
2021-03-10 23:26:22 -0300

    fix(gtm): fixing script tag

--- src/components/GoogleGTM/index.tsx
+++ src/components/GoogleGTM/index.tsx
@@ -10,11 +10,11 @@ export const GTMIframe = styled.iframe<IframeProps>``
 
 export const HeadGTM: React.FC = () => {
   const gtmScript = `<!-- Google Tag Manager -->
-  <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+  (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
   new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
   j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
   'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-  })(window,document,'script','dataLayer','GTM-TGCZZCC');</script>
+  })(window,document,'script','dataLayer','GTM-TGCZZCC');
   <!-- End Google Tag Manager -->`
 
   return <script dangerouslySetInnerHTML={{ __html: gtmScript }} />
